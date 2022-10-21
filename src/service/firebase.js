725cccfdbf8ee54ba0dd7263ff8885d3ce4bb7ca
Suna Choi <sunachoi06@gmail.com>
2022-10-21 16:43:03 +0900

    2022.10.21

---
+++ src/service/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_API_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_API_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_API_STORAGE_BUCKET,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
